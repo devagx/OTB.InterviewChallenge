d2f46562483fd9e2e76bcb75ace6e2bbc201889f
devagx <alungroome@hotmail.com>
2021-01-18 15:59:21 +0000

    Created validation test cases

--- OTB.InterviewChallenge.Tests/JobFactoryTests.cs
+++ OTB.InterviewChallenge.Tests/JobFactoryTests.cs
@@ -10,9 +10,48 @@ namespace OTB.InterviewChallenge.Tests
     [TestFixture]
     public class JobFactoryTests
     {
-        [Test]
-        public void TestMethod1()
+        [SetUp]
+        public void SetUp()
         {
+
         }
+
+        [Test]
+        public void GetSortedJobs__EmptyInputStringArguments_ReturnsPassedValidationResult()
+        {
+            try
+            {
+                string expected = "INVALID STRING";
+
+            }
+            catch (Exception ex)
+            {
+                var expected = "Input string arguments can not be empty";
+                Assert.That(ex.Message, Is.EqualTo(expected));
+            }
+        }
+
+        [Test]
+        public void GetSortedJobs__InvalidInputStringArguments_ReturnsPassedValidationResult()
+        {
+            try
+            {
+
+                string expected = "INVALID STRING";
+            }
+            catch (Exception ex)
+            {
+                var expected = "Input is not in the correct format. Split chars => must exist with a mandatory value on the left of => and an optional value on the right. e.g. a=>b or a=>";
+                Assert.That(ex.Message, Is.EqualTo(expected));
+            }
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+
+        }
+
+
     }
 }
