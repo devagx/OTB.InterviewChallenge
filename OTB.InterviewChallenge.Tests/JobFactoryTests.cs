faf86b337b958cdbb2d09a0e8e7b2c24eef8f794
devagx <alungroome@hotmail.com>
2021-01-18 15:30:44 +0000

    Created base test project using NUnit

---
+++ OTB.InterviewChallenge.Tests/JobFactoryTests.cs
@@ -0,0 +1,18 @@
+using AutoFixture;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using OTB.InterviewChallenge;
+using System;
+
+namespace OTB.InterviewChallenge.Tests
+{
+    [TestFixture]
+    public class JobFactoryTests
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
